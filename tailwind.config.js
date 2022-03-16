8374af7c6471c2f5ebade15db027ed1a083b2c03
michaeloye <michaeloye23@gmail.com>
2022-03-16 03:03:30 +0100

    Add color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-bg": "rgba(255, 255, 255, 0.1)",
+        "primary-gray": "rgba(30, 30, 30, 0.57)",
+      },
+    },
   },
   plugins: [],
 };
