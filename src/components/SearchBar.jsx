10ba4666b773474d4b8fd69a595c8e338187da3f
michaeloye <michaeloye23@gmail.com>
2022-03-16 05:16:53 +0100

    Add state input and link to country input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,15 +3,34 @@ import axios from "axios";
 import { nanoid } from "nanoid";
 
 function SearchBar({ placeholder }) {
-  const [input, setInput] = useState();
+  const [input, setInput] = useState("");
+  const [stateInput, setStateInput] = useState("");
   const [data, setData] = useState([]);
+  const [stateData, setStateData] = useState([]);
 
   function clearInput() {
     setInput("");
   }
   function setInputValue(e) {
     setInput(e.target.value);
+    // initially setting stateInput because if user changes the country the previous states of the
+    // previous country will be shown unless clicked on
+    setStateInput("");
+    // the value needs to be sliced because the target value contains the short name and full name
+    // US - United States of America but what is needed is United States of America for filtering
+    const filtered = data.filter(
+      (country) => country.country == e.target.value.slice(5)
+    );
+    setStateData(filtered[0].cities);
   }
+
+  function clearStateInput() {
+    setStateInput("");
+  }
+  function setStateInputValue(e) {
+    setStateInput(e.target.value);
+  }
+  // The useEffect runs only on initial mount of the app, at this point the data is fetched
   useEffect(() => {
     axios
       .get("https://countriesnow.space/api/v0.1/countries")
@@ -26,13 +45,13 @@ function SearchBar({ placeholder }) {
   }, []);
 
   return (
-    <div>
+    <div className="flex flex-col gap-3 items-center md:justify-center md:flex-row md:gap-10 mt-5">
       <input
         type="text"
-        className="border-2 h-10 px-2 rounded-lg w-3/4 bg-primary-bg"
+        className="border-2 h-10 px-2 md:h-9 rounded-full w-3/4 md:w-1/3 bg-primary-bg text-white"
         list="countries"
         value={input}
-        placeholder={`Select to ${placeholder}`}
+        placeholder={`Select country`}
         onClick={clearInput}
         onChange={setInputValue}
       />
@@ -41,6 +60,23 @@ function SearchBar({ placeholder }) {
           <option key={nanoid()}>
             {country.iso2} - {country.country}
           </option>
+        ))}
+      </datalist>
+
+      {/* The second input is dependent on the first input 'countries' based on the country chosen in the
+      first input the second input is filtered accordingly */}
+      <input
+        type="text"
+        className="border-2 h-10 px-2 md:h-9 rounded-full w-3/4 md:w-1/3 bg-primary-bg text-white"
+        list="states"
+        value={stateInput}
+        placeholder={`Select state`}
+        onClick={clearStateInput}
+        onChange={setStateInputValue}
+      />
+      <datalist id="states">
+        {stateData.map((state) => (
+          <option key={nanoid()}>{state}</option>
         ))}
       </datalist>
     </div>
