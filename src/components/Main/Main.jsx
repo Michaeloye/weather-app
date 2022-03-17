180358edf65c7419d1552426dc47d6055f89435c
michaeloye <michaeloye23@gmail.com>
2022-03-17 15:30:23 +0100

    Add Main.jsx

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,68 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Current from "../Current";
+
+function Main({ state, country }) {
+  const [cityName, setCityName] = useState("");
+  const [date, setDate] = useState("");
+  const [apparentTemperature, setApparentTemperature] = useState("");
+  const [temperature, setTemperature] = useState("");
+  const [weatherIcon, setWeatherIcon] = useState("");
+  const [weatherDescription, setWeatherDescription] = useState("");
+  const [clouds, setClouds] = useState("");
+  const [humidity, setHumidity] = useState("");
+  const [windDetails, setwindDetails] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.weatherbit.io/v2.0/current?city=${state}&country=${country}&key=aa281b7f79634dd9865be4d29994feda`
+      )
+      .then((res) => {
+        const data = res.data.data[0];
+        console.log(data);
+        setCityName(data.city_name);
+        setDate(data.datetime.slice(0, data.datetime.indexOf(":")));
+        setApparentTemperature(data.app_temp);
+        setTemperature(data.temp);
+        setWeatherIcon(data.weather.icon);
+        setWeatherDescription(data.weather.description);
+        setClouds(data.clouds);
+        setHumidity(data.rh);
+        setwindDetails(data.wind_cdir + data.wind_spd);
+      })
+      .catch((err) => console.log(err.message));
+    console.log({
+      cityName,
+      date,
+      apparentTemperature,
+      temperature,
+      weatherIcon,
+      weatherDescription,
+      clouds,
+      humidity,
+      windDetails,
+    });
+    return () => {
+      null;
+    };
+  }, []);
+
+  return (
+    <div>
+      <Current
+        cityName={cityName}
+        date={date}
+        apparentTemperature={apparentTemperature}
+        temperature={temperature}
+        weatherIcon={weatherIcon}
+        weatherDescription={weatherDescription}
+        clouds={clouds}
+        humidity={humidity}
+        windDetails={windDetails}
+      />
+    </div>
+  );
+}
+
+export default Main;
