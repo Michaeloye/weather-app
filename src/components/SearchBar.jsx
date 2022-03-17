0a2d0045beeadfd7956c28fb8baf0c646a6c7797
michaeloye <michaeloye23@gmail.com>
2022-03-17 15:31:08 +0100

    Remove states and functions

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,59 +2,69 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { nanoid } from "nanoid";
 
-function SearchBar({ placeholder }) {
-  const [input, setInput] = useState("");
-  const [stateInput, setStateInput] = useState("");
-  const [data, setData] = useState([]);
-  const [stateData, setStateData] = useState([]);
+function SearchBar({
+  input,
+  stateInput,
+  data,
+  stateData,
+  clearInput,
+  setInputValue,
+  clearStateInput,
+  setStateInputValue,
+}) {
+  // const [input, setInput] = useState("");
+  // const [stateInput, setStateInput] = useState("");
+  // const [data, setData] = useState([]);
+  // const [stateData, setStateData] = useState([]);
 
-  function clearInput() {
-    setInput("");
-  }
-  function setInputValue(e) {
-    setInput(e.target.value);
-    // initially setting stateInput because if user changes the country the previous states of the
-    // previous country will be shown unless clicked on
-    setStateInput("");
-    // the value needs to be sliced because the target value contains the short name and full name
-    // US - United States of America but what is needed is United States of America for filtering
-    const filtered = data.filter(
-      (country) => country.country == e.target.value.slice(5)
-    );
-    setStateData(filtered[0].cities);
-  }
+  // function clearInput() {
+  //   setInput("");
+  // }
+  // function setInputValue(e) {
+  //   setInput(e.target.value);
+  //   // initially setting stateInput because if user changes the country the previous states of the
+  //   // previous country will be shown unless clicked on
+  //   setStateInput("");
+  //   // the value needs to be sliced because the target value contains the short name and full name
+  //   // US - United States of America but what is needed is United States of America for filtering
+  //   const filtered = data.filter(
+  //     (country) => country.country == e.target.value.slice(5)
+  //   );
+  //   setStateData(filtered[0].cities);
+  // }
 
-  function clearStateInput() {
-    setStateInput("");
-  }
-  function setStateInputValue(e) {
-    setStateInput(e.target.value);
-  }
-  // The useEffect runs only on initial mount of the app, at this point the data is fetched
-  useEffect(() => {
-    axios
-      .get("https://countriesnow.space/api/v0.1/countries")
-      .then((res) => {
-        setData(res.data.data);
-        console.log(res.data.data);
-      })
-      .catch((error) => console.log(error.message));
-    return () => {
-      setData([]);
-    };
-  }, []);
+  // function clearStateInput() {
+  //   setStateInput("");
+  // }
+  // function setStateInputValue(e) {
+  //   setStateInput(e.target.value);
+  // }
+  // // The useEffect runs only on initial mount of the app, at this point the data is fetched
+  // useEffect(() => {
+  //   axios
+  //     .get("https://countriesnow.space/api/v0.1/countries")
+  //     .then((res) => {
+  //       setData(res.data.data);
+  //       console.log(res.data.data);
+  //     })
+  //     .catch((error) => console.log(error.message));
+  //   return () => {
+  //     setData([]);
+  //   };
+  // }, []);
 
   return (
     <div className="flex flex-col gap-3 items-center md:justify-center md:flex-row md:gap-10 mt-5">
       <input
         type="text"
-        className="border-2 h-10 px-2 md:h-9 rounded-full w-3/4 md:w-1/3 bg-primary-bg text-white"
+        className="border text-sm outline-none h-10 px-2 md:h-9 rounded-full w-3/4 md:w-1/3 bg-primary-bg text-white"
         list="countries"
         value={input}
         placeholder={`Select country`}
         onClick={clearInput}
         onChange={setInputValue}
       />
+
       <datalist id="countries">
         {data.map((country) => (
           <option key={nanoid()}>
@@ -67,7 +77,7 @@ function SearchBar({ placeholder }) {
       first input the second input is filtered accordingly */}
       <input
         type="text"
-        className="border-2 h-10 px-2 md:h-9 rounded-full w-3/4 md:w-1/3 bg-primary-bg text-white"
+        className="border text-sm h-10 px-2 md:h-9 rounded-full w-3/4 md:w-1/3 bg-primary-bg text-white"
         list="states"
         value={stateInput}
         placeholder={`Select state`}
