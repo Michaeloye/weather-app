828b50744f7be2e0ff9cf3ed78b38b73074cefe3
michaeloye <michaeloye23@gmail.com>
2022-03-19 22:22:12 +0100

    Add Forecast.jsx

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Forecast() {
-  return <div>Forecast</div>;
+  return (
+    <div className="bg-white w-3/4 mx-auto h-96 rounded-3xl mt-10">
+      Forecast
+    </div>
+  );
 }
 
 export default Forecast;
