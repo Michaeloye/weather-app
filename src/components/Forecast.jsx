ea1f1156bb6aa256d308396a67867066e88d8c29
michaeloye <michaeloye23@gmail.com>
2022-03-19 22:34:06 +0100

    Adjust shadow

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -5,7 +5,7 @@ function Forecast() {
     <div
       className="bg-[rgba(255,255,255,0.2)] w-3/4 mx-auto h-96 rounded-3xl mt-10"
       style={{
-        boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
+        boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.07 )",
         backdropFilter: "blur( 4px )",
         WebkitBackdropFilter: "blur( 4px )",
         border: "1px solid rgba( 255, 255, 255, 0.18 )",
