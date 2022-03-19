3c9704b9992717f36dc13893edc99464d2f6af1f
michaeloye <michaeloye23@gmail.com>
2022-03-19 22:33:09 +0100

    Add glassmorphism style

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -2,7 +2,15 @@ import React from "react";
 
 function Forecast() {
   return (
-    <div className="bg-white w-3/4 mx-auto h-96 rounded-3xl mt-10">
+    <div
+      className="bg-[rgba(255,255,255,0.2)] w-3/4 mx-auto h-96 rounded-3xl mt-10"
+      style={{
+        boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
+        backdropFilter: "blur( 4px )",
+        WebkitBackdropFilter: "blur( 4px )",
+        border: "1px solid rgba( 255, 255, 255, 0.18 )",
+      }}
+    >
       Forecast
     </div>
   );
